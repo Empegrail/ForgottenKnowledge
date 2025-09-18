db21fdced1435468dc9c97d569abbd5cb4fe3818
Empegrail <bloonsgold@gmail.com>
2025-09-18 18:06:36 +0200

    Initial commit

---
+++ src/client/java/empegrail/forgotten_knowledge/ForgottenKnowledgeClient.java
@@ -0,0 +1,10 @@
+package empegrail.forgotten_knowledge;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ForgottenKnowledgeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/empegrail/forgotten_knowledge/ForgottenKnowledge.java
@@ -0,0 +1,15 @@
+package empegrail.forgotten_knowledge;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ForgottenKnowledge implements ModInitializer {
+    public static final String MOD_ID = "forgotten_knowledge";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
